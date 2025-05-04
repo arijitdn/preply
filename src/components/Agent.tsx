0236f8ee1e2f55e992c00aafa8233a0d9dafcc52
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-05-05 00:50:42 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Agent.tsx
+++ src/components/Agent.tsx
@@ -167,7 +167,7 @@ const Agent = ({
         <div className="card-border">
           <div className="card-content">
             <Image
-              src="/user-avatar.png"
+              src="/avatar.png"
               alt="profile-image"
               width={539}
               height={539}
